834ef7af8ac1c5470f2af85473eba2ea52b83d6c
tranminhhoa2005 <148558253+tranminhhoa2005@users.noreply.github.com>
2026-03-28 08:05:09 +0700

    Delete Trần Minh Hòa/3.3 directory

--- Trần Minh Hòa/3.3/bai2.py
+++
@@ -1,8 +0,0 @@
-_a = int(input("Nhập cạnh a: "))
-_b = int(input("Nhập cạnh b: "))
-_c = int(input("Nhập cạnh c: "))
-
-if _a + _b > _c and _a + _c > _b and _b + _c > _a:
-    print("Đây là độ dài ba cạnh tam giác")
-else:
-    print("Đây không phải độ dài ba cạnh tam giác")
--- Trần Minh Hòa/3.3/bai3.py
+++
@@ -1,8 +0,0 @@
-import time
-
-x = time.localtime()
-year = x[0]
-
-_nam_sinh = int(input("Nhập năm sinh: "))
-_tuoi = year - _nam_sinh
-print(f"Năm sinh {_nam_sinh}, vậy bạn {_tuoi} tuổi.")
